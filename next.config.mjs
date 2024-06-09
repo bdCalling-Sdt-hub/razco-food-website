0bb44015b1b6eb241e31943427f636bec5353dc6
Nadir Hossain <nadirhossain336@gmail.com>
2024-06-09 12:42:03 +0600

    ssl add in server

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
     images: {
         remotePatterns: [
           {
-            protocol: 'http',
+            protocol: 'https',
             hostname: 'server.razcofoods.net',
             pathname: '**',
           },
