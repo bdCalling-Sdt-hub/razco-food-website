0f7d1275c33b07a76c0ac991a399beb325ddaa4b
Nadir Hossain <nadirhossain336@gmail.com>
2024-08-20 15:51:47 +0600

    refactor: :recycle: refactor code

--- src/Config.jsx
+++ src/Config.jsx
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 export const baseURL = axios.create({
-    // baseURL: "https://server.razcofoods.net/api/v1",
-    baseURL: "http://192.168.10.185:5000/api/v1",
+    baseURL: "https://server.razcofoods.net/api/v1",
+    // baseURL: "http://192.168.10.185:5000/api/v1",
     timeout: 10000,
     headers: {'X-Custom-Header': 'foobar'}
 });
-// export const ImageConfig = "https://server.razcofoods.net"
-export const ImageConfig = "http://192.168.10.185:5000"
+export const ImageConfig = "https://server.razcofoods.net"
+// export const ImageConfig = "http://192.168.10.185:5000"
