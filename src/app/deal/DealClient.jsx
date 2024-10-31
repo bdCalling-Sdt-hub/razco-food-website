f86c7b25084aa515bde43b0c473b499140c2ec08
Nadir Hossain <nadirhossain336@gmail.com>
2024-10-31 17:30:43 +0600

    refactor: :recycle: refactor code

--- src/app/deal/DealClient.jsx
+++ src/app/deal/DealClient.jsx
@@ -1,6 +1,6 @@
 'use client';
-import { Pagination } from 'antd'
-import React, { useEffect } from 'react'
+import { ConfigProvider, Modal } from 'antd'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { getWeeklyDeal } from "@/redux/apiSlice/getWeeklyOfferSlice";
 import Image from 'next/image';
@@ -8,33 +8,67 @@ import { ImageConfig } from '@/Config';
 
 const DealClient = () => {
     const dispatch = useDispatch();
-    const { WeeklyDeals } = useSelector(state=> state.getDeal);
+    const { WeeklyDeals } = useSelector(state => state.getDeal);
+    const [Open, setOpen] = useState(null)
 
-    useEffect(()=>{
+    useEffect(() => {
         dispatch(getWeeklyDeal())
-    }, [dispatch]);  
+    }, [dispatch]);
     return (
         <div className='container my-10'>
             <h1 className='font-bold text-2xl mb-5'>Weekly Deals</h1>
-            <div className='flex items-center gap-4 flex-wrap'>
+            <div className='flex items-center gap-6 flex-wrap'>
                 {
-                    WeeklyDeals?.map((deal, index)=>{
+                    WeeklyDeals?.map((deal, index) => {
                         return (
-                            <div 
+                            <div
                                 key={index}
-                                className="relative w-[220px] h-[220px] overflow-hidden rounded" 
+                                onClick={() => setOpen(deal)}
+                                className="relative border mx-auto sm:mx-0 cursor-pointer p-1 w-[220px] h-[220px] overflow-hidden rounded"
                             >
-                                <Image 
-                                    src={`${ImageConfig}${deal?.image}`} 
+                                <Image
+                                    src={`${ImageConfig}${deal?.image}`}
                                     alt="offer image"
                                     fill
-                                    style={{objectFit: "cover"}}
+                                    style={{ objectFit: "cover" }}
                                 />
                             </div>
                         )
                     })
                 }
             </div>
+            <ConfigProvider
+                theme={{
+                    components: {
+                        Modal: {
+                            wireframe: true,
+                        }
+                    }
+                }}
+            >
+                <Modal
+                    centered
+                    open={Open}
+                    onCancel={() => setOpen(null)}
+                    footer={null}
+                    footerStyle={{ padding: 0 }}
+                    width={500}
+                    closeIcon={false}
+                    bodyStyle={{ padding: 0 }}
+                >
+                    <div
+                        className="relative border overflow-hidden rounded"
+                        style={{ width: 500, height: 500, padding: 0 }} // Remove inner div padding
+                    >
+                        <Image
+                            src={`${ImageConfig}${Open?.image}`}
+                            alt="offer image"
+                            fill
+                            style={{ objectFit: "cover" }}
+                        />
+                    </div>
+                </Modal>
+            </ConfigProvider>
         </div>
     )
 }
